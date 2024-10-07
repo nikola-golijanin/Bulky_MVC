c26a72ea78f54d35d64741100b7bbbfba0a6790a
nikola-golijanin <golijanin.nikola@outlook.com>
2024-10-07 17:32:39 +0200

    Added simple category controller, dbContext, migrations and Category index view

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>().HasData(
+            new { Id = 1, Name = "Action", DisplayOrder = 1 },
+            new { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+            new { Id = 3, Name = "History", DisplayOrder = 3 }
+            );
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -1,7 +1,14 @@
+using BulkyWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("PostgresConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
+
 
 var app = builder.Build();
 
