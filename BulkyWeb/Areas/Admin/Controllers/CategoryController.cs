595553570f4033a855e4635544a6df4d39bb86f1
Nikola Golijanin <nikola.golijanin@dinitcs.com>
2024-10-09 14:42:08 +0200

    Refactor controllers to use areas
    Refactored `CategoryController` and `HomeController` to use areas, moving them to `Admin` and `Customer` areas respectively. Updated `Program.cs` and `_Layout.cshtml` to include `area` parameter in routes and navigation links. Re-added views for `Category` and `Home` controllers with original content.

--- BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
+++ BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -2,8 +2,9 @@
 using Domain.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BulkyWeb.Controllers;
+namespace BulkyWeb.Areas.Admin.Controllers;
 
+[Area("Admin")]
 public class CategoryController : Controller
 {
     private readonly ICategoryRepository _categoryRepository;
---
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BulkyWeb.Areas.Customer.Controllers;
+
+[Area("Customer")]
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index() => View();
+
+    public IActionResult Privacy() => View();
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+        => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+}
