a77567b9e8d9e9929972a325abc64b6df526ebe3
Nikola Golijanin <nikola.golijanin@dinitcs.com>
2024-10-21 15:40:36 +0200

    Add Service.UnitTests project with initial test setup
    A new project named `Service.UnitTests` has been added to the solution `Bulky.sln`.
    
    The `Service.UnitTests.csproj` targets `.NET 8.0` and includes several NuGet package references for testing:
    - `coverlet.collector` version `6.0.0`
    - `Microsoft.NET.Test.Sdk` version `17.8.0`
    - `xunit` version `2.5.3`
    - `xunit.runner.visualstudio` version `2.5.3`
    
    The project file also includes a `Using` directive for `Xunit`.

---
+++ Service.UnitTests/CompanyServiceTests.cs
@@ -0,0 +1,15 @@
+namespace Service.UnitTests;
+
+public class CompanyServiceTests
+{
+    [Fact]
+    public void GetAll_Should_ReturnSuccess()
+    {
+        //Arrange
+
+        //Act
+
+        //Assert
+        Assert.True(true);
+    }
+}
