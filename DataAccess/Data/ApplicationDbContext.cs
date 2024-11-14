b06571db1a56a6bf5f335603108eb852fdec41c5
Nikola Golijanin <nikola.golijanin@dinitcs.com>
2024-11-14 15:25:25 +0100

    Add OrderHeader and OrderDetail entities
    Added OrderHeader and OrderDetail entities to ApplicationDbContext.
    Created migration to add OrderHeaders and OrderDetails tables.
    
    Defined OrderHeader with properties and foreign key relationships.

--- DataAccess/Data/ApplicationDbContext.cs
+++ DataAccess/Data/ApplicationDbContext.cs
@@ -14,6 +14,8 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     public DbSet<Product> Products { get; set; }
     public DbSet<Company> Companies { get; set; }
     public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+    public DbSet<OrderHeader> OrderHeaders { get; set; }
+    public DbSet<OrderDetail> OrderDetails { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
---
+++ Domain/Models/OrderDetail.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Models;
+public class OrderDetail
+{
+    public int Id { get; set; }
+
+    public int OrderHeaderId { get; set; }
+
+    [ForeignKey("OrderHeaderId")]
+    [ValidateNever]
+    public OrderHeader OrderHeader { get; set; }
+
+    public int ProductId { get; set; }
+
+    [ForeignKey("ProductId")]
+    [ValidateNever]
+    public Product Product { get; set; }
+
+    public int Count { get; set; }
+
+    public double Price { get; set; }
+}
