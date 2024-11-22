d3bf10e648c3fe6cfc1b131d52e98243388c967f
Nikola Golijanin <nikola.golijanin@dinitcs.com>
2024-11-22 10:53:07 +0100

    Refactor config keys and update environment settings
    Move UseInMemoryDb from env variable to appSettings

--- BulkyWeb/Configuration/DatabaseServiceInstaller.cs
+++ BulkyWeb/Configuration/DatabaseServiceInstaller.cs
@@ -6,19 +6,24 @@ namespace BulkyWeb.Configuration;
 
 public class DatabaseServiceInstaller : IServiceInstaller
 {
-	public void Install(IServiceCollection services, IConfiguration configuration)
-	{
-		if (configuration.GetValue<bool>("USE_INMEMORY_DB"))
-			RegisterInMemoryDatabase(services);
-		else
-			RegisterPostgresDatabase(services, configuration);
-	}
+    public void Install(IServiceCollection services, IConfiguration configuration)
+    {
+        // var useOnlyInMemoryDatabase = configuration["UseOnlyInMemoryDatabase"];
+        var useOnlyInMemoryDatabase = configuration.GetValue<bool>("UseOnlyInMemoryDatabase");
 
-	private static void RegisterPostgresDatabase(IServiceCollection services, IConfiguration configuration) =>
-		services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("PostgresConnection")));
+        if (useOnlyInMemoryDatabase)
+        {
+            RegisterInMemoryDatabase(services);
+            return;
+        }
+        else RegisterPostgresDatabase(services, configuration);
+    }
 
-	private static void RegisterInMemoryDatabase(IServiceCollection services) =>
-		services
-			.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("InMemoryDb")
-			.LogTo(Console.WriteLine));
+    private static void RegisterPostgresDatabase(IServiceCollection services, IConfiguration configuration) =>
+        services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("PostgresConnection")));
+
+    private static void RegisterInMemoryDatabase(IServiceCollection services) =>
+        services
+            .AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("InMemoryDb")
+            .LogTo(Console.WriteLine));
 }
--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -7,15 +7,15 @@ builder.Services.InstallServices(configuration: builder.Configuration, typeof(IS
 
 var app = builder.Build();
 
-if (builder.Configuration.GetValue<bool>("USE_INMEMORY_DB"))
-	app.Services.ApplyMigrationsToInMemoryDB();
+if (builder.Configuration.GetValue<bool>("UseOnlyInMemoryDatabase"))
+    app.Services.ApplyMigrationsToInMemoryDB();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-	app.UseExceptionHandler("/Home/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -31,7 +31,7 @@ app.UseAuthorization();
 app.MapRazorPages();
 
 app.MapControllerRoute(
-	name: "default",
-	pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
+    name: "default",
+    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
